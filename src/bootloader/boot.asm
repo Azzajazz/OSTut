30a5efbb6f4a6e4bfe8d60b1b9346fb9e350d88f
Azzajazz <azzajazz1@gmail.com>
2025-03-02 21:09:25 +1100

    Restore bootloader code

--- src/bootloader/boot.asm
+++ src/bootloader/boot.asm
@@ -16,55 +16,30 @@ start:
 
 welcome_msg: db "Bootloading OSTut", CR, LF, 0
 memory_enumerate_msg: db "Found %dKB before EBDA", CR, LF, 0
-tst_msg: db "%d %x %d", CR, LF, 0
 newline_msg: db CR, LF, 0
 
 %include "io.asm"
 
 boot:
-    push 123 
-    push 0xfff
-    push 65535
-    push tst_msg
-    call rm_print_fmt_16
-    add sp, 8
-    push 'a'
-    call rm_print_char
-    add sp, 2
     push welcome_msg
-    call rm_print_string
-    add sp, 2
-    push 0x4555
-    call rm_print_hex_16
-    add sp, 2
-    push newline_msg
-    call rm_print_string
-    add sp, 2
-    push 123
-    call rm_print_dec_16
-    add sp, 2
-    push newline_msg
-    call rm_print_string
+    call rm_print_fmt_16
     add sp, 2
 
-    ;mov di, welcome_msg
-    ;call rm_print_fmt_16
+    int 0x12
+    push ax
+    push memory_enumerate_msg
+    call rm_print_fmt_16
+    add sp, 4
 
-    ;int 0x12
-    ;push ax
-    ;mov di, memory_enumerate_msg
-    ;call rm_print_fmt_16
-    ;add sp, 2
-
-    ;mov ah, 0x02
-    ;mov al, 1
-    ;mov ch, 0
-    ;mov cl, 2
-    ;mov dh, 0
-    ;mov dl, 0
-    ;mov bx, 0x50
-    ;int 0x13
-    ;jmp 0x500
+    mov ah, 0x02
+    mov al, 1
+    mov ch, 0
+    mov cl, 2
+    mov dh, 0
+    mov dl, 0
+    mov bx, 0x50
+    int 0x13
+    jmp 0x500
 
     cli
     hlt
